fc0d5fb7347adcf88de90e1b1c065d4bf4e8b107
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-26 19:12:55 +0700

    up

---
+++ react-movies/src/movies/IndividualMovie.tsx
@@ -0,0 +1,19 @@
+import { movieDTO } from "./movies.model";
+import css from "./IndividualMovie.module.css"
+
+export default function IndividualMovie(props: movieDTO) {
+  const buildLink = () => {
+    return `/movie/${props.id}`;
+  };
+
+  return (
+    <div className={css.div}>
+      <a href={buildLink()}>
+        <img src={props.poster} alt="Poster" />
+      </a>
+      <p>
+        <a href={buildLink()}>{props.title}</a>
+      </p>
+    </div>
+  );
+}
