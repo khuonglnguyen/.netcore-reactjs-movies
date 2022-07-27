496e1e126245953edd724c495753076b758c9230
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-27 11:56:22 +0700

    Added loading

--- react-movies/src/movies/MoviesList.tsx
+++ react-movies/src/movies/MoviesList.tsx
@@ -1,17 +1,24 @@
+import Loading from "../utils/Loading";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./movies.model";
-import css from "./MoviesList.module.css"
+import css from "./MoviesList.module.css";
 
 export default function MoviesList(props: moviesListProps) {
-  return (
-    <div className={css.div}>
-      {props.movies.map((movie) => {
-        return <IndividualMovie {...movie} key={movie.id }/>;
-      })}
-    </div>
-  );
+  if (!props.movies) {
+    return <Loading />
+  } else if (props.movies.length === 0) {
+    return <>There are no movies to display</>;
+  } else {  
+    return (
+      <div className={css.div}>
+        {props.movies.map((movie) => {
+          return <IndividualMovie {...movie} key={movie.id} />;
+        })}
+      </div>
+    );
+  }
 }
 
 interface moviesListProps {
-  movies: movieDTO[];
+  movies?: movieDTO[];
 }
---
+++ react-movies/src/utils/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+    return (
+        <img src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw340" alt="" />
+    )
+}
