fc25acadd62c8041b76a1c7ad42950fcfe6638fb
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-27 12:05:32 +0700

    Added generic list component

--- react-movies/src/movies/MoviesList.tsx
+++ react-movies/src/movies/MoviesList.tsx
@@ -1,22 +1,19 @@
+import GenericList from "../utils/GenericList";
 import Loading from "../utils/Loading";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./movies.model";
 import css from "./MoviesList.module.css";
 
 export default function MoviesList(props: moviesListProps) {
-  if (!props.movies) {
-    return <Loading />
-  } else if (props.movies.length === 0) {
-    return <>There are no movies to display</>;
-  } else {  
-    return (
+  return (
+    <GenericList list={props.movies}>
       <div className={css.div}>
-        {props.movies.map((movie) => {
+        {props.movies?.map((movie) => {
           return <IndividualMovie {...movie} key={movie.id} />;
         })}
       </div>
-    );
-  }
+    </GenericList>
+  );
 }
 
 interface moviesListProps {
