232dca2c8bcd51f1d6527e7975f21916bd020ef0
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-28 15:19:00 +0700

    demo useHistory

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,7 +1,15 @@
+import { useHistory } from "react-router-dom";
+import Button from "../utils/Button";
+
 export default function CreateGenre() {
+  const history = useHistory();
   return (
     <>
       <h3>Create Genre</h3>
+      <Button onClieck={()=>{
+        // saving...
+        history.push('/genres')
+      }}>Save Changes</Button>
     </>
   );
 }
--- react-movies/src/utils/Button.tsx
+++ react-movies/src/utils/Button.tsx
@@ -1,7 +1,12 @@
 export default function Button(props: buttonProps) {
-  return <button className="btn btn-primary">{props.children}</button>;
+  return (
+    <button className="btn btn-primary" onClick={props.onClieck}>
+      {props.children}
+    </button>
+  );
 }
 
 interface buttonProps {
   children: React.ReactNode;
+  onClieck(): void;
 }
