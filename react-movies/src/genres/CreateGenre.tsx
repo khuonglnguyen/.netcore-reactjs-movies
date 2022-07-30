94fe2e80db762634c0f166b6d11d61255c262f6b
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-30 19:43:28 +0700

    Added edit form

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,42 +1,16 @@
-import { Form, Formik } from "formik";
-import { Link } from "react-router-dom";
-import Button from "../utils/Button";
-import * as Yup from "yup";
-import TextField from "../forms/TextField";
+import GenreForm from "./GenreForm";
 
 export default function CreateGenre() {
   return (
     <>
       <h3>Create Genre</h3>
-
-      <Formik
-        initialValues={{
-          name: "",
-        }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .required("This field is required")
-            .firstLetterUppercase(),
-        })}
+      <GenreForm
+        model={{ name: "" }}
         onSubmit={async (value) => {
-          await new Promise((r) => setTimeout(r, 3000));
+          await new Promise((r) => setTimeout(r, 1));
           console.log(value);
-          
         }}
-      >
-        {(formikProps) => (
-          <Form>
-            <TextField field="name" displayName="Name"></TextField>
-
-            <Button disabled={formikProps.isSubmitting} type="submit">
-              Save Changes
-            </Button>
-            <Link className="btn btn-secondary" to="/genres">
-              Cancel
-            </Link>
-          </Form>
-        )}
-      </Formik>
+      ></GenreForm>
     </>
   );
 }
---
+++ react-movies/src/genres/GenreForm.tsx
@@ -0,0 +1,41 @@
+import { Form, Formik, FormikHelpers } from "formik";
+import { Link } from "react-router-dom";
+import TextField from "../forms/TextField";
+import Button from "../utils/Button";
+import * as Yup from "yup";
+import { genreCreationDTO } from "./genres.model";
+
+export default function GenreForm(props: genreFormProps) {
+  return (
+    <Formik
+      initialValues={props.model}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .required("This field is required")
+          .firstLetterUppercase(),
+      })}
+      onSubmit={props.onSubmit}
+    >
+      {(formikProps) => (
+        <Form>
+          <TextField field="name" displayName="Name"></TextField>
+
+          <Button disabled={formikProps.isSubmitting} type="submit">
+            Save Changes
+          </Button>
+          <Link className="btn btn-secondary" to="/genres">
+            Cancel
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+interface genreFormProps {
+  model: genreCreationDTO;
+  onSubmit(
+    value: genreCreationDTO,
+    action: FormikHelpers<genreCreationDTO>
+  ): void;
+}
