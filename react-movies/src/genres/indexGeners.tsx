b89c6fb5155cbd92a93616819dd84a6daf03c0c8
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-04 15:36:28 +0700

    010 Index Genres

--- react-movies/src/genres/indexGeners.tsx
+++ react-movies/src/genres/indexGeners.tsx
@@ -1,16 +1,17 @@
 import axios, { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { urlGenres } from "../endpoints";
+import GenericList from "../utils/GenericList";
 import { genreDTO } from "./genres.model";
 
 export default function IndexGeners() {
+  const [genres, setGenres] = useState<genreDTO[]>();
+
   useEffect(() => {
-    axios
-      .get(urlGenres)
-      .then((response: AxiosResponse<genreDTO[]>) =>
-        console.log(response.data)
-      );
+    axios.get(urlGenres).then((response: AxiosResponse<genreDTO[]>) => {
+      setGenres(response.data);
+    });
   }, []);
 
   return (
@@ -19,6 +20,26 @@ export default function IndexGeners() {
       <Link to="/genres/create" className="btn btn-primary">
         Create genre
       </Link>
+      <GenericList list={genres}>
+        <table className="table table-striped">
+          <thead>
+            <th></th>
+            <th>Name</th>
+          </thead>
+          <tbody>
+            {genres?.map((genre) => (
+              <tr key={genre.id}>
+                <td>{genre.name}</td>
+                <td>
+                  <Link className="btn btn-success" to={`/genres/${genre.id}`}>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </GenericList>
     </>
   );
 }
