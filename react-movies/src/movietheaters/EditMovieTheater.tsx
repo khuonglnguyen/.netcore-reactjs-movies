9c7ec9d4bb2bd19d13df3a46cd9a29afff4c2319
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-09 13:43:02 +0700

    Added Edit and Delete Movie Theater

--- react-movies/src/movietheaters/EditMovieTheater.tsx
+++ react-movies/src/movietheaters/EditMovieTheater.tsx
@@ -1,17 +1,21 @@
+import { urlMovieTheaters } from "../endpoints";
+import EditEntity from "../utils/EditEntity";
+import { movieTheaterCreationDTO, movieTheaterDTO } from "./movieTheater.model";
 import MovieTheaterForm from "./MovieTheaterForm";
 
 export default function EditMovieTheater() {
   return (
-    <>
-      <h3>Edit Movie Theater</h3>
-      <MovieTheaterForm
-        model={{
-          name: "Hello",
-          latitude: 10.042880099928833,
-          longitude: 105.76653169072291,
-        }}
-        onSubmit={(values) => console.log(values)}
-      ></MovieTheaterForm>
-    </>
+    <EditEntity<movieTheaterCreationDTO, movieTheaterDTO>
+      url={urlMovieTheaters}
+      indexURL="/movietheaters"
+      entityName="Movie Theater"
+    >
+      {(entity, edit) => (
+        <MovieTheaterForm
+          model={entity}
+          onSubmit={async (values) => await edit(values)}
+        ></MovieTheaterForm>
+      )}
+    </EditEntity>
   );
 }
