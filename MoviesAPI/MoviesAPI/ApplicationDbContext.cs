4ee7b0ef8138298d89e1a75d4c69e5de1798449a
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-09 14:03:30 +0700

    Added Movies Entities

--- MoviesAPI/MoviesAPI/ApplicationDbContext.cs
+++ MoviesAPI/MoviesAPI/ApplicationDbContext.cs
@@ -9,8 +9,20 @@ namespace MoviesAPI
         public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoviesActors>().HasKey(x => new { x.ActorId, x.MovieId });
+            modelBuilder.Entity<MoviesGenres>().HasKey(x => new { x.MovieId, x.GenreId });
+            modelBuilder.Entity<MovieTheatersMovies>().HasKey(x => new { x.MovieId, x.MovieTheaterId });
+
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Actor> Actors { get; set; }
         public DbSet<MovieTheater> MovieTheaters { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+        public DbSet<MovieTheatersMovies> MovieTheatersMovies { get; set; }
     }
 }
